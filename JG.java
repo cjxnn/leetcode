2a41afe957ebcbb70a0ce2311d939733e6ccb71e
Niannan Xue <nx310@ubu30gb-templ.doc.ic.ac.uk>
2016-09-01 15:39:06 +0100

    Add solution JG
    Add solution to Jump Game

---
+++ JG.java
@@ -0,0 +1,22 @@
+/** 
+ * Given an array of non-negative integers, you are initially positioned at the first index of the array.
+ * Each element in the array represents your maximum jump length at that position.
+ * Determine if you are able to reach the last index.
+ * For example:
+ * A = [2,3,1,1,4], return true.
+ * A = [3,2,1,0,4], return false.
+ */
+
+public class Solution {
+    public boolean canJump(int[] nums) {
+        int max = nums[0];
+        for(int i = 1; i < nums.length; i++){
+            if (max == 0)
+                return false;
+            else
+                max = Math.max(max - 1, nums[i]);
+        }
+        
+        return true;
+    }
+}
