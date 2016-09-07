1caf6bf08a141d2be574e260b1c7a839b6af8abd
Niannan Xue <nx310@ubu30gb-templ.doc.ic.ac.uk>
2016-09-08 00:41:59 +0100

    Add solution BAoNR
    Complete solution to Bitwise AND of Numbers Range

---
+++ BAoNR.java
@@ -0,0 +1,11 @@
+/**
+ * Given a range [m, n] where 0 <= m <= n <= 2147483647, return the bitwise AND of all numbers in this range, inclusive.
+ * For example, given the range [5, 7], you should return 4.
+ */
+
+public class Solution {
+    public int rangeBitwiseAnd(int m, int n) {
+        while (m < n) n -= n & -n;
+        return n;
+    }
+}
